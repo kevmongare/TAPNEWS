6b61cb38732b24d7368b5bc19ba7d10033a5be2c
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-02-12 11:41:30 +0300

    Am adding my base files for my site

---
+++ TAPNEWS.js
@@ -0,0 +1,4 @@
+document.getElementById("Uploadpage").addEventListener("click", function()//this statement helps me get the id of the button and we add an event listener where by anytime someone clicks the js function is triggered
+{
+window.location.href = "TAPNEWSUPLOAD.html"; //under our case the function triggered is the button links to a page for uploading video files 
+});
